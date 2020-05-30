05cf9763394a0b4ed3be6bd6862113a1c989fa0f
alexander <pancasancha4@gmail.com>
2020-05-30 21:10:17 +0300

    Base Test Settings -add setUp method in base settings class

---
+++ src/test/java/BaseDNSTest.java
@@ -0,0 +1,67 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class BaseDNSTest {
+	private static WebDriver driver;
+	private static Properties properties = SingleProperty.getInstance().getProperties();
+
+	@BeforeClass
+	public static void setUp() {
+		setUpBrowser(System.setProperty("browser", "chrome"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		driver.quit();
+	}
+
+	private static void setUpBrowser(String browserName) {
+		String[] osName = System.getProperty("os.name").toLowerCase().split("\\s");
+		switch (osName[0]) {
+			case "linux":
+				switch (browserName) {
+					case "chrome":
+						System.setProperty(properties.getProperty("driverChrome"), properties.getProperty("pathToDriverChromeLin"));
+						driver = new ChromeDriver();
+						break;
+					case "firefox":
+						System.setProperty(properties.getProperty("driverFirefox"), properties.getProperty("pathToDriverFirefoxLin"));
+						driver = new FirefoxDriver();
+						break;
+				}
+				break;
+			case "windows":
+				switch (browserName) {
+					case "chrome":
+						System.setProperty(properties.getProperty("driverChrome"), properties.getProperty("pathToDriverChromeWin"));
+						driver = new ChromeDriver();
+						break;
+					case "firefox":
+						System.setProperty(properties.getProperty("driverFirefox"), properties.getProperty("pathToDriverFirefoxWin"));
+						driver = new FirefoxDriver();
+						break;
+				}
+				break;
+			case "mac":
+				switch (browserName) {
+					case "chrome":
+						System.setProperty(properties.getProperty("driverChrome"), properties.getProperty("pathToDriverChromeMac"));
+						driver = new ChromeDriver();
+						break;
+					case "firefox":
+						System.setProperty(properties.getProperty("driverFirefox"), properties.getProperty("pathToDriverFirefoxMac"));
+						driver = new FirefoxDriver();
+						break;
+				}
+				break;
+		}
+	}
+}

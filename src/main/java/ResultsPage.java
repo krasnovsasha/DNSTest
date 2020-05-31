5ba4490cdcda874acac010524d613d1617d9470b
alexander <pancasancha4@gmail.com>
2020-05-31 05:11:49 +0300

    -page with results of finding items

---
+++ src/main/java/ResultsPage.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class ResultsPage extends BasePage {
+	public ResultsPage(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(xpath = "//span[text()='Наличие в магазинах']")
+	private WebElement presentInShops;
+	@FindBy(xpath = "//a[contains(text(),'PlayStation 4 Slim Black')]")
+	private WebElement product;
+
+	public WebElement getProduct() {
+		return product;
+	}
+
+	private ResultsPage waitLoading() {
+		wait.until(ExpectedConditions.visibilityOf(presentInShops));
+		wait.until(ExpectedConditions.elementToBeClickable(presentInShops));
+		return new ResultsPage(driver);
+	}
+
+	public ProductPage chooseProduct(WebElement element) {
+		waitLoading();
+		element.click();
+		return new ProductPage(driver);
+	}
+}
